ee4716500a40526a369c40e9a6b424f0b1e5052d
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-14 13:34:16 -0700

    fail test for triangle validator.
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- TriangleTracker.Tests/ModelTests/Triangle.Tests.cs
+++ TriangleTracker.Tests/ModelTests/Triangle.Tests.cs
@@ -1,16 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using TriangleTracker.Models;
 
-namespace TriangleTrackerProject.Tests
+namespace TriangleTracker.Tests
 {
   [TestClass]
   public class TriangleTests
   {
     [TestMethod]
-    public void HasOneSide_ReceivingALengthValue_True()
+    public void TriangleConstructor_ReceivingCorrectSideValues_3()
     {
-      Triangle testTriangle = new Triangle();
-      Assert.AreEqual(true, testTriangle);
+      Triangle testTriangle = new Triangle(3,3,3);
+      Assert.AreEqual(3, testTriangle.SideOne);
+      Assert.AreEqual(3, testTriangle.SideTwo);
+      Assert.AreEqual(3, testTriangle.SideThree);
+    }
+
+    [TestMethod]
+    public void TriangleValidator_ReceivingCorrectTrianlgeType_NotATriangle()
+    {
+      Triangle testTriangle = new Triangle(1,2,5);
+      Assert.AreEqual("Not a triangle!", testTriangle.TriangleValidator());
     }
   }
 }
+
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
